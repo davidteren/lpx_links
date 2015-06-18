97ac14428bc71c9457f6afadca112717f671ab25
David Teren <dteren@gmail.com>
2015-06-18 22:34:01 +0200

    fix typo

--- lpx_links.rb
+++ lpx_links.rb
@@ -6,4 +6,4 @@ require 'json'
 `plutil -convert json \'#{PLIST}\' -o #{JSN}`
 JSON.parse(File.read(JSN))[PKG].each {|i| ; @line << "#{File.join(URL, i[1][DLN])}\n"}
 f = File.open(LST, 'a') ;  f.puts @line.sort ; f.close
-puts "Done! Found #{@line.to_a.length} links.\nCheck the following file:\n#{LSTs}"
+puts "Done! Found #{@line.to_a.length} links.\nCheck the following file:\n#{LST}"
