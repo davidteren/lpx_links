1f1bd3dd87aefc55061498f3827393196fece0f5
David Teren <dteren@gmail.com>
2015-08-17 22:09:01 +0200

    re-factor code

--- lpx_links.rb
+++ lpx_links.rb
@@ -4,6 +4,13 @@ require_relative './lib/config'
 require 'json'
 
 `plutil -convert json \'#{PLIST}\' -o #{JSN}`
-JSON.parse(File.read(JSN))[PKG].each {|i| ; @line << "#{File.join(URL, i[1][DLN])}\n"}
-f = File.open(LST, 'w') ;  f.puts @line.sort ; f.close ; File.delete(JSN)
-puts "Done! Found #{@line.to_a.length} links.\nCheck the following file:\n#{LST}"
+JSON.parse(File.read(JSN))[PKG].each do |i|
+  @line << "#{File.join(URL, i[1][DLN])}\n"
+end
+f = File.open(LST, 'w')
+f.puts @line.sort
+f.close
+File.delete(JSN)
+puts "Done! Found #{@line.to_a.length} links.
+Check the following file:
+#{LST}"
