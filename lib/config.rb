d711fb2b7e763e1f0d9145453c2e21e77d8a4e27
David Teren <dteren@gmail.com>
2015-08-28 00:12:15 +0200

    added new dir structure

--- lib/config.rb
+++ lib/config.rb
@@ -1,11 +1,15 @@
 TMPDIR  = '/tmp'
-APP     = '/Applications/Logic Pro X.app'
+LPX     = '/Applications/Logic Pro X.app'
 RSC     = '/Contents/Resources/logicpro1020.plist'
 URL     = 'http://audiocontentdownload.apple.com/lp10_ms3_content_2015/'
-DIRNME  = File.dirname(__FILE__)
-PLIST   = File.join(APP, RSC)
+ROOT  	= File.dirname(__FILE__)
+p ROOT
+PARENT	= File.join(ROOT, '..', '..')
+PLIST   = File.join(LPX, RSC)
 JSN     = File.join(TMPDIR, 'content.json')
-LST     = File.join(DIRNME, '..', 'links.txt')
+DWN_LNK = File.join(PARENT, 'download_links')
+DWN_LST = File.join(DWN_LNK, 'links.txt')
+INSTALS = File.join(PARENT, 'installers')
 PKG     = 'Packages'
 DLN     = 'DownloadName'
 @line   = []
