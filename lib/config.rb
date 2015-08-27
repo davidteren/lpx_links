e2615ac3e15a7f44d5474e64ef9c6ccb1bf8dbd9
David Teren <dteren@gmail.com>
2015-08-28 00:48:57 +0200

    add parent path & report file

--- lib/config.rb
+++ lib/config.rb
@@ -3,13 +3,13 @@ LPX     = '/Applications/Logic Pro X.app'
 RSC     = '/Contents/Resources/logicpro1020.plist'
 URL     = 'http://audiocontentdownload.apple.com/lp10_ms3_content_2015/'
 ROOT  	= File.dirname(__FILE__)
-p ROOT
-PARENT	= File.join(ROOT, '..', '..')
+PARENT	= File.expand_path("..", Dir.pwd)
 PLIST   = File.join(LPX, RSC)
 JSN     = File.join(TMPDIR, 'content.json')
 DWN_LNK = File.join(PARENT, 'download_links')
 DWN_LST = File.join(DWN_LNK, 'links.txt')
 INSTALS = File.join(PARENT, 'installers')
+REPORT	= File.join(DWN_LNK, 'report.txt')
 PKG     = 'Packages'
 DLN     = 'DownloadName'
 @line   = []
