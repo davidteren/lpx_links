607b270e0faa4ab2f85431615d3399c19dbc2ede
AlftheElf <45872124+AlftheElf@users.noreply.github.com>
2023-04-15 20:06:18 +0100

    Update download_install.sh (#24)
    A mismatch with forked versions meant that packages became corrupted, and couldn't be installed. This fixes that issue.

--- download_install.sh
+++ download_install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-cd $2
-while read -r line; do
-    name="$line"
-    echo "Downloading $name..."
-    curl -O $name
-done < $1
-
-for pkg in $2/*.pkg; do
+for pkg in $1/*.pkg; do
     echo "Installing $pkg file..."
     installer -pkg $pkg -target /
 done
-
