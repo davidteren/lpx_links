9b5efed949f13f6de6547dcb50ef019e015b90c7
davidteren <drteren@gmail.com>
2025-10-01 19:11:40 +0200

    Add Minitest infrastructure and migrate tests from RSpec
    - Add minitest, minitest-reporters, and rake gems to Gemfile
    - Create test/ directory structure with test_helper.rb
    - Create Rakefile for running tests
    - Update GitHub Actions CI to run minitest
    - Migrate all RSpec tests to Minitest format:
      - test/lpx_links_test.rb (3 tests)
      - test/lib/file_helpers_test.rb (6 tests)
    - All 9 tests passing with 62.89% coverage
    - RSpec tests still passing for validation

--- Gemfile
+++ Gemfile
@@ -2,5 +2,8 @@
 
 source 'https://rubygems.org'
 
+gem 'minitest', '~> 5.20'
+gem 'minitest-reporters', '~> 1.6'
+gem 'rake', '~> 13.0'
 gem 'rspec', '~> 3.12'
 gem 'simplecov', '~> 0.22.0', require: false
---
+++ test/lpx_links_test.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class LpxLinksTest < Minitest::Test
+  def setup
+    @sample_packages = {
+      'Packages' => {
+        'package1' => {
+          'DownloadName' => 'mandatory_package.pkg',
+          'IsMandatory' => true
+        },
+        'package2' => {
+          'DownloadName' => 'optional_package.pkg',
+          'IsMandatory' => false
+        }
+      }
+    }
+
+    File.stub(:read, @sample_packages.to_json) do
+      FileHelpers.stub(:url, 'http://example.com/content/') do
+        # Setup complete
+      end
+    end
+  end
+
+  def test_download_links_returns_all_links_when_mandatory_flag_is_false
+    File.stub(:read, @sample_packages.to_json) do
+      FileHelpers.stub(:url, 'http://example.com/content/') do
+        links = LpxLinks.download_links(only_mandatory: false)
+        
+        assert_equal 2, links.length
+        assert_includes links, "http://example.com/content/mandatory_package.pkg\n"
+        assert_includes links, "http://example.com/content/optional_package.pkg\n"
+      end
+    end
+  end
+
+  def test_download_links_returns_only_mandatory_links_when_mandatory_flag_is_true
+    File.stub(:read, @sample_packages.to_json) do
+      FileHelpers.stub(:url, 'http://example.com/content/') do
+        links = LpxLinks.download_links(only_mandatory: true)
+        
+        assert_equal 1, links.length
+        assert_includes links, "http://example.com/content/mandatory_package.pkg\n"
+      end
+    end
+  end
+
+  def test_read_packages_correctly_parses_json_file
+    File.stub(:read, @sample_packages.to_json) do
+      packages = LpxLinks.read_packages
+      
+      assert_equal @sample_packages['Packages'], packages
+    end
+  end
+end
+
